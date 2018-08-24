aab839df409a51bcf7f5c50e8d2f26a516ce461d
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-08-24 10:30:38 +0400

    Add create view

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h1>Create Post</h1>
+				
+			</div>
+		</div>
+	</div>
+@endsection
