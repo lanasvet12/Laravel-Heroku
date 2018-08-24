036de032c8fe8f457b3f566aaf6330756c0eeee5
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-08-24 11:33:20 +0400

    Fix interpretator html code by CKEditor in file show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
 			<div class="col-12">
 				<a class="btn btn-default" href="/posts">Go back</a>
 				<h1 class="text-center">{{ $post->title }}</h1>
-				<p>{{ $post->body }}</p>
+				<p>{!! $post->body !!}</p>
 			</div>
 		</div>
 	</div>
