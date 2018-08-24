97ad30ed8076e8894196c865dbd65c70358dd71d
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-08-24 14:59:25 +0400

    Fix index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,7 @@
 					<div class="card">
 				  		<div class="card-body">
 				    		<h5 class="card-title"><a href="/posts/{{$post->id}}">{{ $post->title }}</a></h5>
-				    		<p class="card-text">{{ $post->body }}</p>
+				    		<p class="card-text">{!! $post->body !!}</p>
 				  		</div>
 					</div>
 					@endforeach
