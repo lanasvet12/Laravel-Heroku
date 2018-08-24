241479f2b7a716a0defe33ad91eec751fa998f95
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-08-24 13:43:32 +0400

    Add auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/services', 'PagesController@services');
 
 
 Route::resource('posts', 'PostsController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
