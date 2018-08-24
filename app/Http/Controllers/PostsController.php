d968147d8cff8c9f5b085aa9325ebb51ceb1fa17
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-08-24 16:08:56 +0400

    Add logic upload images files

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -48,13 +48,32 @@ class PostsController extends Controller
     {
         $this->validate($request, [
             'title' => 'required',
-            'body' => 'required'
+            'body' => 'required',
+            'cover_image' => 'image|nullable|max:1999'
         ]);
+
+        // Handle File Upload 
+
+        if ($request->hasFile('cover_image')) {
+            // Get filename with the extension
+            $fileNameWithExt = $request->file('cover_image')->getClientOriginalImage();
+            // Get just filename 
+            $filename = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            $extension = $request->file('cover_image')->getOriginalClientExtension();
+            // Filename to store
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+            // Upload image
+            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+        } else {
+            $fileNameToStore = 'noimage.jpg'
+        }
 
         $post = new Post;
         $post->title = $request->input('title');
         $post->body = $request->input('body');
         $post->user_id = auth()->user()->id;
+        $post->cover_image = $fileNameToStore;
+        
         $post->save();
 
         return redirect('/posts')->with('success', 'Post Created');
