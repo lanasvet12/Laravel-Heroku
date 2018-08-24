6baf1cb5e97bac83585c107e27a2cfe199c3d768
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-08-24 16:59:39 +0400

    Fix

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\File;
+use Illuminate\Http\Controllers\File;
 use App\Post;
 
 class PostsController extends Controller
