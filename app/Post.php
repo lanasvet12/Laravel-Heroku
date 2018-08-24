b44a13435c21b94f84a11d27dd1bee7c71e15669
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-08-24 14:28:18 +0400

    Fix Post model

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = 'posts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'token', 
+        'user_id', 
+        'role',
+    ];
 }
