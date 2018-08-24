fd79c4fef915e9e1fe063cec39adabec6b858297
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-08-24 11:05:19 +0400

    hot fix in PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -46,7 +46,7 @@ class PostsController extends Controller
         $post->body = $request->input('body');
         $post->save();
 
-        return redirect('/posts')-with('success', 'Post Created');
+        return redirect('/posts')->with('success', 'Post Created');
     }
 
     /**
