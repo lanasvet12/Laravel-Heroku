209ea3c826ba1e3e28719dccaa0899b8e855eaf5
livevasiliy <vasiliy.mv.obzor@gmail.com>
2018-08-24 11:07:09 +0400

    Hot fix orderBy posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderBy('create_at', 'desc')->paginate(1);
+        $posts = Post::orderBy('created_at', 'desc')->paginate(1);
         return view('posts.index')->with('posts', $posts);
     }
 
